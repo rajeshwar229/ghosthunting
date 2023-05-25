aa255b79f5608028fb174a765602dde864d786f9
Rajeshwar <rajeshwar229@gmail.com>
2023-05-25 23:25:44 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-// JavaScript Document
+/********* START OF SCRIPT *********/
+
 $(document).ready(function(e){
 	
 	var ghostIndex = 0, ghostCreateInterval, shootInterval, score = 0, scoreFlag = false, winFlag = false, finalScore = 0, volume=true, sound=true;
@@ -19,6 +20,36 @@ $(document).ready(function(e){
 		else{
 			x.play();
 		}
+	}
+
+	const hideShowPage = function(hidePage, showPage, hidePageTime, showPageTime){
+		$(`.${hidePage}`).fadeOut(hidePageTime);
+		$(`.${showPage}`).fadeIn(showPageTime);
+	}
+
+	const gameResult = function(resultText, finalScore, hscore){
+		$(".gameResult p[data-name='game-result']").html(resultText);
+		$(".gameResult p[data-name=score]").html(`Score: ${finalScore}`);
+		$(".gameResult p[data-name='high-score']").html(`HighScore: ${hscore}`);
+		jQuery.fx.off = true;	
+	}
+
+	const resetGame = function() {
+		gameMusic.currentTime = 0;
+		ghostCry.currentTime = 0;
+		entranceMusic.currentTime = 0;
+		ghostCry.pause();
+		score = 0;
+		finalScore = 0;
+		ghostIndex = 0;
+		scoreFlag = false;
+		winFlag = false;
+		ghost.create();
+		$(".score").html(`Score: ${score=0}`);
+		$(".blud").css("width",'100%');
+		clearInterval(ghostCreateInterval);
+		clearTimeout(shootInterval);
+		jQuery.fx.off=false;
 	}
 
 	const ghostMove = function(left, top, height, width, backSize, oldLeft, oldTop) {
@@ -44,9 +75,13 @@ $(document).ready(function(e){
 				if($(".game").is(':visible')){
 					$(".game").append(`<div data-name='ghost${ghostIndex}'></div>`);
 					$(`div[data-name=ghost${ghostIndex}]`).append($("<div/>").addClass("blood"+ghostIndex));
+					let topAdjust = 325;
+					if(window.innerHeight < 480){
+						topAdjust = 150;
+					}
 					$(`div[data-name=ghost${ghostIndex}]`).css({
 						"left":Math.random()*(window.innerWidth-366)+100,
-						"top":Math.random()*(window.innerHeight-325),
+						"top":Math.random()*(window.innerHeight-topAdjust),
 					});
 					let oldLeft= $(`div[data-name=ghost${ghostIndex}]`).position().left;
 					let oldTop=$(`div[data-name=ghost${ghostIndex}]`).position().top;
@@ -59,7 +94,7 @@ $(document).ready(function(e){
 					setTimeout(function(){				
 						if($this.children().width()>0){
 							$this.animate({
-									backgroundSize:'100%',
+									backgroundSize:'90%',
 									width:'+=25%',
 									height:'+=25%',
 								},100);
@@ -76,7 +111,6 @@ $(document).ready(function(e){
 							}
 						},7000);
 						ghostIndex++;
-						winFlag=false;
 
 					if(ghostIndex === 10){
 						clearInterval(ghostCreateInterval);
@@ -88,95 +122,67 @@ $(document).ready(function(e){
 			},3000);				
 		},
 		over:function(){
-			if($(".blud").width()<=0){
+			if($(".blud").width() <= 0){
 				$(".game").fadeOut(100);
 				$(".result").fadeIn(2000);
 				gameMusic.pause();
-				gameMusic.currentTime=0;
+				gameMusic.currentTime = 0;
 				ghostCry.play();
-				$(".gameResult p[data-name='game-result']").html("GAME OVER ");
-				$(".gameResult p[data-name=score]").html("score:"+finalScore);
-				$(".gameResult p[data-name='high-score']").html("HighScore:"+localStorage.hscore);
-				jQuery.fx.off=true;
+				gameResult("GAME OVER", finalScore, localStorage.hscore);
 				$("div[data-name^=ghost]").remove();				
 			}
 			$(".retry").click(function(){
-				gameMusic.currentTime = 0;
+				resetGame();
 				gameMusic.play();
-				ghostCry.pause();
-				ghostCry.currentTime=0;
-				clearInterval(ghostCreateInterval);
-				clearTimeout(shootInterval);
-				jQuery.fx.off=false;
-				$(".blud").css("width",'100%');
-				ghostIndex = 0;
-				ghost.create();
-				$(".result").toggle();
-				$(".game").show();
-				$(".score").html(`Score: ${score=0}`);
-				$(".blud").css("background","blue");
-				scoreFlag = false;
-				winFlag = false;
-				finalScore=0;
+				hideShowPage('result', 'game', 500, 500);
 			});
-			
-			if($(".blud").width()>500){$(".blud").css("background","blue");}
-			if($(".blud").width()<=500){$(".blud").css("background","green");}
-			if($(".blud").width()<=300){$(".blud").css("background","red");}
-		},
-
-		score:function(){
-			if(scoreFlag){
-			$(".score").html(`Score: ${score=score+100}`);
-			scoreFlag = false;
-			finalScore = score;
-			}
-			if(localStorage){
-				if(localStorage.hscore === null || localStorage.hscore === undefined){
-						localStorage.hscore = 0;
-						localStorage.hscore=(score>localStorage.hscore?score:localStorage.hscore);
-				}
-				else{
-					localStorage.hscore= (score > localStorage.hscore ? score : localStorage.hscore);
-				}
-			}else{
-				$(".gameResult p[data-name='high-score']").hide()
-			}
 		},
 
 		shoot:function(){
 			setInterval(function(){
 				$("div[data-name^=ghost]").each(function(){
-					let $this=$(this);
-					if(winFlag){
+					let $this = $(this);
+					if(winFlag && $(".blud").width() > 0){
 						shootInterval = setTimeout(function(){
-							if($(".blud").width() > 0){
-								$(".game").fadeOut("slow");
-								$(".result").show();
+								hideShowPage('game', 'result', 500, 500);
 								$(`div[data-name^=ghost]:last-child`).remove();
 								gameMusic.pause();
 								gameMusic.currentTime=0;
 								ghostCry.play();
-								$(".gameResult p[data-name='game-result']").html("YOU WON ");
-								$(".gameResult p[data-name=score]").html("score:"+finalScore);
-								$(".gameResult p[data-name='high-score']").html("HighScore:"+localStorage.hscore);
-								jQuery.fx.off = true;
+								gameResult("You Won", finalScore, localStorage.hscore);
 								scoreFlag = false;
 								winFlag = false;
 								$(".score").html(`Score: ${score=0}`);
 								$("div[data-name^=ghost]").remove();
-							}
 						},500);
 					}	
+
 					$this.on("click",function(){
 						$this.children().css("width",'-=3').show();
-						if($this.children().width()<=0){
+						if($this.children().width() <= 0){
 							$this.stop().fadeOut(1);
 							scoreFlag = true;
 							ghostDie.play();
 						}
 					});
-					ghost.score();
+					
+					if(scoreFlag){
+						$(".score").html(`Score: ${score = score+100}`);
+						scoreFlag = false;
+						finalScore = score;
+					}
+					if(localStorage){
+						if(localStorage.hscore === null || localStorage.hscore === undefined){
+								localStorage.hscore = 0;
+								localStorage.hscore=(score>localStorage.hscore?score:localStorage.hscore);
+						}
+						else{
+							localStorage.hscore= (score > localStorage.hscore ? score : localStorage.hscore);
+						}
+					}
+					else{
+						$(".gameResult p[data-name='high-score']").hide()
+					}
 				});
 			},1000);
 		},
@@ -192,94 +198,96 @@ $(document).ready(function(e){
 		ghost.shoot();
 		gun.move();
 		
-	 $(".play").click(function(){
-		$(".game").fadeIn(1500);
-		$(".entrance").slideUp(500);
-		$(".helpPage").slideUp(200);
-		$(".score").html("Score: "+(score=0));
-		finalScore=0;
-		
+	$(".play").click(function(){
+		hideShowPage('entrance', 'game', 200, 500);
 		gameMusic.play();
-		$(document).on("click",function(){
+		$(document).on("click",function(e){
 			if($(".game").is(':visible')){
-				$(".gunLight").fadeIn(100).fadeOut(400);
+				$(".bulletFire").css({
+					left:(e.pageX-0)+"px",top:(e.pageY-0)+"px"
+				}).fadeIn(10).fadeOut(200);
 				shootSound(gunShoot);
 			}	
 		});
 		entranceMusic.pause();
-		entranceMusic.CurrentTime=0;
-			
 	});
+
 	$(".credits").click(function(){
-		$(".entrance").fadeOut(1);
-		$(".creditsPage").fadeIn(100);
+		hideShowPage('entrance', 'creditsPage', 200, 300);
 	 });
 
 	$(".help").click(function(){
-		$(".helpPage").slideDown(300);
-		$(".entrance").slideUp(200);
+		hideShowPage('entrance', 'helpPage', 200, 300);
 	});
 	
-	$(".back").click(function(){
-		$(".helpPage").slideDown(200);
-		$(".entrance").slideToggle(300);
-		$(".creditsPage").fadeOut(1000);
+	$(".creditsPage .back").click(function(){
+		hideShowPage('creditsPage', 'entrance', 200, 300);
 	});
 
+	$(".helpPage .back").click(function(){
+		hideShowPage('helpPage', 'entrance', 200, 300);
+	});
+
+	$('.full-screen-btn').on('click',function(){
+		let de = document.documentElement;
+		if(this.dataset.fullscreen === 'off'){
+				if(de.requestFullscreen){
+						de.requestFullscreen();
+				}
+				else if(de.mozRequestFullscreen){de.mozRequestFullscreen();}
+				else if(de.webkitRequestFullscreen){de.webkitRequestFullscreen();}
+				else if(de.msRequestFullscreen){de.msRequestFullscreen();}
+				screen.orientation.lock('landscape');
+				$(this).attr("data-fullscreen","on")
+				$(this).text("Exit Full Screen");
+		}
+		else {
+				screen.orientation.unlock();
+				if(document.fullscreen){
+				document.exitFullscreen();
+				}
+				$(this).attr("data-fullscreen","off")
+				$(this).text("Full Screen");
+		}
+		});
+
 	$(".menu").click(function(){
-		$(".entrance").show(300);
-		$(".result").fadeOut(200);
-		clearTimeout(ghostCreateInterval);
-		jQuery.fx.off=false;
-		$(".blud").css("width",'100%');
-		ghostIndex = 0;
-		ghost.create(); 
+		resetGame();
 		entranceMusic.play();
-		ghostCry.pause();
-		ghostCry.currentTime=0;
+		hideShowPage('result', 'entrance', 200, 300);
 	});	
 	
 	// Intro Animation
-	$(".intro span[data-name=Rajeshwar]").animate({top:'40%'},500,function(){
-		for(let i=3;i>0;i--){
-			$(this).animate({top:'-='+(i*10)},100).animate({top:'+='+(i*10)},150);
+	$(".intro span[data-name=Rajeshwar]").animate({top:'40%'}, 500, function(){
+		for(let i = 3; i > 0; i--){
+			$(this).animate({top: '-='+(i*10)},100).animate({top: '+='+(i*10)},150);
 		}
-		$(".intro span[data-name=Chinna]").animate({left:'50%'},300,function(){
-			for(let i=2;i>0;i--){
-				$(this).animate({left:'+='+(i*10)},100).animate({left:'-='+(i*10)},100);
+		$(".intro span[data-name=Chinna]").animate({left: '50%'}, 300, function(){
+			for(let j = 2; j > 0; j--){
+				$(this).animate({left:'+='+(j*10)}, 100).animate({left: '-='+(j*10)}, 100);
 			}
 			$(".intro span[data-name=Presents]").animate({left:'61%'},500,function(){
-				$(".intro").fadeOut(1000);
+				hideShowPage('intro', 'entrance', 1000, 500);
 				setTimeout(function(){
-					$(".entrance").fadeIn(1000);
 					if($(".entrance").is(':visible')){
-					entranceMusic.play();
+						entranceMusic.play();
 					}
 				},1000);
 			});
 		});
 	});	
-	
-	$(document).on("click",function(e){
-		if($(".game").is(':visible')){
-			$(".bulletFire").css({
-				left:(e.pageX-0)+"px",top:(e.pageY-0)+"px"
-			}).fadeIn(10).fadeOut(200);
-			shootSound(gunShoot);
-		}
-	});
-						
+			
 	$(".mute").on("click",function(){
 		if(!volume){
-			entranceMusic.volume=1;
-			gameMusic.volume=1;
-			ghostCry.volume=1;
+			entranceMusic.volume = 1;
+			gameMusic.volume = 1;
+			ghostCry.volume = 1;
 			$(this).removeClass("volume").attr("title","Music On");
 		}
 		if(volume){
-			entranceMusic.volume=0;
-			gameMusic.volume=0;
-			ghostCry.volume=0;
+			entranceMusic.volume = 0;
+			gameMusic.volume = 0;
+			ghostCry.volume = 0;
 			$(this).addClass("volume").attr("title","Music Off");
 		}
 		volume = !volume;	
@@ -298,8 +306,8 @@ $(document).ready(function(e){
 			gunShoot.volume = 0;
 			$(this).addClass("soundOff").attr("title","Sound Off");
 			}
-		sound=!sound;	
+		sound = !sound;	
 	});
 });
-	
-	
+
+/********* END OF SCRIPT *********/
